41fdc1f2d3aa4304101e534e689e996c48e11923
Mba Thomas <angelfab631@gmail.com>
2024-06-22 19:16:37 +0000

    Create head.php

---
+++ app/views/admin/inc/head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+   <?php require_once APPROOT . "/views/inc/links.php"; ?>
+   <link rel="stylesheet" href="<?= URLROOT ?>/assets/admin/css/style.css" />
+</head>
+
+<body>
