e02aa143d38d2d36f3474516803ce417fd672312
Mba Thomas <angelfab631@gmail.com>
2024-06-22 19:17:12 +0000

    Create head.php

---
+++ app/views/inc/head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+  <meta name="description" content="Freetown hotel and shortlet apartment expect a seamless and tranquil luxury travel experience. Upon reservation, look forward to a serene retreat at our prestigious hotel nestled in the heart of Benin, where relaxation is paramount.">
+  <meta name="keywords" content="Hotel hotel apartment short-let shortlet room benin city sapele No.1 best hotels in benin city ">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php require_once APPROOT . '/views/inc/links.php' ?>
+</head>
