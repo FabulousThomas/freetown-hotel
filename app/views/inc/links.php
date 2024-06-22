8a4bfe0ab8bba723f2cd8a55b9e36d55d1fe4112
Mba Thomas <angelfab631@gmail.com>
2024-06-22 19:17:18 +0000

    Create links.php

---
+++ app/views/inc/links.php
@@ -0,0 +1,9 @@
+<link rel="shortcut icon" href="<?= URLROOT ?>/assets/images/logo/logo-4.png" type="image/x-icon">
+<link rel="stylesheet" href="<?= URLROOT ?>/assets/css/style.css">
+<link rel="stylesheet" href="<?= URLROOT ?>/assets/css/bootstrap5.2.3.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+ <!-- FONTAWESOME -->
+ <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" />
+ <!-- LINE AWESOME -->
+<link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+<link rel="stylesheet" href="<?= URLROOT ?>/assets/css/swipper-10.2.0.css" />
