a16db02327adf7b80436a4ea7b3e9171999ff2de
Mba Thomas <angelfab631@gmail.com>
2024-06-22 19:17:28 +0000

    Create script-links.php

---
+++ app/views/inc/script-links.php
@@ -0,0 +1,19 @@
+<script src="<?= URLROOT ?>/assets/js/jquery3.7.1.js"></script>
+<script src="<?= URLROOT ?>/assets/js/htmx.org.js"></script>
+<script src="<?= URLROOT ?>/assets/js/sweet-alert.js"></script>
+<script src="<?= URLROOT ?>/assets/js/bootstrap5.2.3.min.js"></script>
+<script src="<?= URLROOT ?>/assets/js/swipper-10.2.0.js"></script>
+<script src="<?= URLROOT ?>/assets/js/filter.js"></script>
+<!-- Initializer Swiper -->
+<script src="<?= URLROOT ?>/assets/js/swipper-script.js"></script>
+
+<script>
+   function showPassword() {
+      var x = document.getElementById("show_pass");
+      if (x.type === "password") {
+         x.type = "text";
+      } else {
+         x.type = "password";
+      }
+   }
+</script>
