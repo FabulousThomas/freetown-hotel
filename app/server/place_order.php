9c90ac36d636d0a3f72fa3a6d125b949c8f861ed
Mba Thomas <angelfab631@gmail.com>
2025-06-09 19:47:46 -0600

    Create place_order.php

---
+++ app/server/place_order.php
@@ -0,0 +1,28 @@
+<?php
+
+// if (isset($_POST['btn_pay_now'])) {
+
+//    // $data = filteration($_POST);
+//    // global $con;
+
+//    // // GET USERS INFO AND STORE TO DATABASE
+//    // $status = 'not paid';
+//    // $days = $_SESSION['days'];
+//    // $user_id = $_SESSION['user_id'];
+//    // $cost = $_SESSION['room']['payment'];
+//    // $user_email = $_SESSION['user_email'];
+//    // $room_id = $_SESSION['room']['room_id'];
+//    // $booking_id = random_int(1111111, 9999999);
+
+//    // $stmt = $con->prepare("INSERT INTO `bookings`(`booking_id`, `user_id`, `room_id`, `cost`, `days`, `status`, `name`, `email`, `phone`, `check_in`, `check_out`, `address`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
+
+//    // $stmt->bind_param('iiiiisssssss', $booking_id, $user_id, $room_id, $cost, $days, $status, $data['name'], $user_email, $data['phone'], $data['check_in'], $data['check_out'], $data['address']);
+//    // $stmt->execute();
+
+//    // $booking_id = $stmt->insert_id;
+
+//    // $_SESSION['r_id'] = $_SESSION['room']['room_id'];
+
+//    // $_SESSION['booking_id'] = $booking_id;
+//    // redirect('payment.php');
+// }
